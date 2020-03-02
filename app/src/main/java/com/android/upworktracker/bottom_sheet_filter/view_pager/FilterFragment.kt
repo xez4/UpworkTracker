2e18ea2f6003335416e055b34248db8aba2112c1
Ovsaynka <mimiomi138@gmail.com>
2020-03-02 17:29:16 +0200

    update Button

--- app/src/main/java/com/android/upworktracker/bottom_sheet_filter/view_pager/FilterFragment.kt
+++ app/src/main/java/com/android/upworktracker/bottom_sheet_filter/view_pager/FilterFragment.kt
@@ -13,8 +13,8 @@ import kotlinx.android.synthetic.main.filter_list.*
 class FilterFragment : Fragment() {
 
     override fun onCreateView(
-            inflater: LayoutInflater, container: ViewGroup?,
-            savedInstanceState: Bundle?
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.filter_list, container, false)
     }
@@ -43,10 +43,10 @@ class FilterFragment : Fragment() {
         }
 
         val experienceLevelData = arrayListOf(
-                "Any experience level",
-                "Entry Level - $",
-                "Intermediate - $$",
-                "Master - $$$"
+            "Any experience level",
+            "Entry Level - $",
+            "Intermediate - $$",
+            "Master - $$$"
         )
 
         val experienceLevelAdapter = RadioAdapter(experienceLevelData)
@@ -56,7 +56,7 @@ class FilterFragment : Fragment() {
         }
 
         val clientHistoryData =
-                arrayListOf("Any client history", "No hires", "1 to 9 hires", "10+ hires")
+            arrayListOf("Any client history", "No hires", "1 to 9 hires", "10+ hires")
         val clientHistoryAdapter = RadioAdapter(clientHistoryData)
         clientHistoryRecyclerView.apply {
             layoutManager = LinearLayoutManager(view.context)
@@ -64,12 +64,12 @@ class FilterFragment : Fragment() {
         }
 
         val numberOfProposalsData = arrayListOf(
-                "Any Number of Proposals",
-                "Less than 5",
-                "5 to 10",
-                "10 to 15",
-                "15 to 20",
-                "20 to 50"
+            "Any Number of Proposals",
+            "Less than 5",
+            "5 to 10",
+            "10 to 15",
+            "15 to 20",
+            "20 to 50"
         )
         val numberOfProposalsAdapter = RadioAdapter(numberOfProposalsData)
         numberOfProposalsRecyclerView.apply {
@@ -78,12 +78,12 @@ class FilterFragment : Fragment() {
         }
 
         val budgetData = arrayListOf(
-                "Any Budget",
-                "Less than $100",
-                "$100 - $500",
-                "$500 - $1k",
-                "$1k - $5k",
-                "$5k+"
+            "Any Budget",
+            "Less than $100",
+            "$100 - $500",
+            "$500 - $1k",
+            "$1k - $5k",
+            "$5k+"
         )
         val budgetAdapter = RadioAdapter(budgetData)
         budgetRecyclerView.apply {
@@ -99,7 +99,7 @@ class FilterFragment : Fragment() {
         }
 
         val hoursPerWeekData =
-                arrayListOf("Any Hours Per Week", "Less than 30 hrs/week", "More than 30 hrs/week")
+            arrayListOf("Any Hours Per Week", "Less than 30 hrs/week", "More than 30 hrs/week")
         val hoursPerWeekAdapter = RadioAdapter(hoursPerWeekData)
         hoursPerWeekRecyclerView.apply {
             layoutManager = LinearLayoutManager(view.context)
