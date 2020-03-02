cf19d4add3a5e672e57761790e0a67be63715b34
Vasiliy Subbota <kennixer@gmail.com>
2020-03-02 17:21:27 +0200

    Implementing fullscreen for bottom sheet.

--- app/src/main/java/com/android/upworktracker/bottom_sheet_filter/BottomSheet.kt
+++ app/src/main/java/com/android/upworktracker/bottom_sheet_filter/BottomSheet.kt
@@ -1,24 +1,28 @@
 package com.android.upworktracker.bottom_sheet_filter
 
 import android.content.DialogInterface
+import android.graphics.Color
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.coordinatorlayout.widget.CoordinatorLayout
 import com.android.upworktracker.R
 import com.android.upworktracker.bottom_sheet_filter.view_pager.CategoryFragment
 import com.android.upworktracker.bottom_sheet_filter.view_pager.FilterFragment
 import com.android.upworktracker.bottom_sheet_filter.view_pager.ViewPagerFilterAdapter
+import com.google.android.material.bottomsheet.BottomSheetBehavior
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import com.google.android.material.tabs.TabLayoutMediator
 import kotlinx.android.synthetic.main.bottom_sheet_view_pager.*
 
+
 class BottomSheet : BottomSheetDialogFragment() {
 
     override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
+            inflater: LayoutInflater,
+            container: ViewGroup?,
+            savedInstanceState: Bundle?
     ): View? {
         super.onCreateView(inflater, container, savedInstanceState)
         return inflater.inflate(R.layout.bottom_sheet_view_pager, container, false)
@@ -39,7 +43,24 @@ class BottomSheet : BottomSheetDialogFragment() {
                 else -> "category"
             }
         }.attach()
+    }
 
+    override fun onStart() {
+        super.onStart()
+
+        dialog.let {
+            idBottomSheet.layoutParams.height = ViewGroup.LayoutParams.MATCH_PARENT
+        }
+
+        val view = view
+        view?.post {
+            val parent = view.parent as View
+            val params = parent.layoutParams as CoordinatorLayout.LayoutParams
+            val behavior = params.behavior
+            val bottomSheetBehavior = behavior as BottomSheetBehavior
+            bottomSheetBehavior.peekHeight = view.measuredHeight
+            (idBottomSheet.parent as View).setBackgroundColor(Color.TRANSPARENT)
+        }
     }
 
     override fun onDismiss(dialog: DialogInterface) {
