756abaa862a832f693850c7551ca159fed04d3a3
Ovsaynka <mimiomi138@gmail.com>
2020-03-12 11:02:38 +0200

    cloud messaging(Firebase)

---
+++ app/src/main/java/com/android/upworktracker/services/MessageService.kt
@@ -0,0 +1,62 @@
+package com.android.upworktracker.services
+
+import android.annotation.SuppressLint
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.media.RingtoneManager
+import android.util.Log
+import androidx.constraintlayout.widget.Constraints.TAG
+import androidx.core.app.NotificationCompat
+import com.android.upworktracker.R
+import com.android.upworktracker.UpworkTracker
+import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
+
+
+class MessageService: FirebaseMessagingService() {
+
+    private val id = "MessageID"
+
+    @SuppressLint("LongLogTag")
+    override fun onMessageReceived(remoteMessage: RemoteMessage) {
+        Log.d(TAG, "Dikirim dari: ${remoteMessage.from}")
+
+        if (remoteMessage.notification != null) {
+            showNotification(remoteMessage.notification?.title, remoteMessage.notification?.body)
+        }
+    }
+
+    private fun showNotification(title: String?, body: String?) {
+        val intent = Intent(this, UpworkTracker::class.java)
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+        val pendingIntent = PendingIntent.getActivity(
+            this, 0, intent,
+            PendingIntent.FLAG_ONE_SHOT
+        )
+
+        val soundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)
+        val notificationBuilder = NotificationCompat.Builder(this,id)
+            .setSmallIcon(R.mipmap.ic_launcher)
+            .setContentTitle(title)
+            .setContentText(body)
+            .setAutoCancel(true)
+            .setSound(soundUri)
+            .setContentIntent(pendingIntent)
+
+        val notificationManager =
+            getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        notificationManager.notify(0, notificationBuilder.build())
+    }
+
+    override fun onNewToken(token: String) {
+        Log.d(TAG, "Refreshed token: $token")
+        sendRegistrationToServer(token)
+    }
+
+    private fun sendRegistrationToServer(token: String?) {
+        Log.d(TAG, "sendRegistrationTokenToServer($token)")
+    }
+
+}
